b2abb4ded50dea5aff1362c4cb8d2d40c9391856
Kang Liu (刘糠) <719176062@qq.com>
2026-03-29 10:34:00 +0800

    Update free-text-report-generation-llm.sh

--- script/free-text-report-generation-llm.sh
+++ script/free-text-report-generation-llm.sh
@@ -6,10 +6,9 @@ CUDA_VISIBLE_DEVICES=0 python ../main_v0702_v0904.py \
 --phase "finetune" \
 --data_name "mimic_cxr" \
 --version "stage2-v0702-v0904-llm" \
---ann_path "six_work_mimic_cxr_annotation_similar_case_v0702_v0826.json" \
+--ann_path "cogaze_mimic_cxr_annotation_similar_case_v0702_v0826.json" \
 --images_dir "MIMIC-CXR/files" \
 --view_position_path "/MIMIC-CXR/view-positions-dict.json" \
---eye_gaze_dir "/MIMIC-Eye-Gaze-Heatmap" \
 --batch_size 6 \
 --test_batch_size 6 \
 --num_workers 10 \
@@ -19,9 +18,9 @@ CUDA_VISIBLE_DEVICES=0 python ../main_v0702_v0904.py \
 --max_length 100 \
 --accumulate_grad_batches 2 \
 --learning_rate 5.0e-5 \
---load "finetune_stage2-v0702-v0905-not-precision_2025_09_05_18-best/checkpoint/best_model.pt" \
+--load "https://huggingface.co/MK-runner/CoGaze/blob/main/mimic_pretrain_best_model.pt" \
 --save_best_model "yes" \
 --save_last_model 'no' \
 --llm_use_lora "yes" \
---ckpt_zoo_dir "/checkpoints" \
+--ckpt_zoo_dir "ckpt_zoo_dir/" \
 --max_epochs 30
