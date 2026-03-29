38b985640a0bf83b9d703d0150e8f932ea66d404
Kang Liu (刘糠) <719176062@qq.com>
2026-03-29 10:36:17 +0800

    Update structured-report-generation-gpt2.sh

--- script/structured-report-generation-gpt2.sh
+++ script/structured-report-generation-gpt2.sh
@@ -6,7 +6,7 @@ CUDA_VISIBLE_DEVICES=3 python ../main_v0702_v0904_srrg.py \
 --phase "finetune" \
 --data_name "srrg" \
 --version "srrg-beam10-3-devices" \
---ann_path "six_work_srrg_annotation_v0702_v0826.json" \
+--ann_path "cogaze_srrg_annotation_v0702_v0826.json" \
 --mimic_dir "/MIMIC-CXR" \
 --chexpert_dir "/dataset/CheXper-Plus/PNG" \
 --view_position_path "/MIMIC-CXR/view-positions-dict.json" \
@@ -19,8 +19,8 @@ CUDA_VISIBLE_DEVICES=3 python ../main_v0702_v0904_srrg.py \
 --max_length 150 \
 --accumulate_grad_batches 2 \
 --learning_rate 5.0e-5 \
---load "/script/results/mimic_cxr/report-generation/finetune_v0702-v0826-final-v1014-beam10_2025_10_14_17/checkpoint/best_model.pt" \
+--load "https://huggingface.co/MK-runner/CoGaze/blob/main/distilgpt2_mimic_free_text_report_generation_best_model.pt" \
 --save_best_model "yes" \
 --save_last_model 'no' \
---ckpt_zoo_dir "/checkpoints" \
+--ckpt_zoo_dir "ckpt_zoo_dir/" \
 --max_epochs 40
