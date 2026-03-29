0dcd32feb8b00548e4db33b35228bd048b993a8d
Kang Liu (刘糠) <719176062@qq.com>
2026-03-29 10:31:19 +0800

    Update free-text-report-generation-gpt2.sh

--- script/free-text-report-generation-gpt2.sh
+++ script/free-text-report-generation-gpt2.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=1 python ../main_v0702_v0826.py \
 --phase "finetune" \
 --data_name "mimic_cxr" \
 --version "v0702-v0826-final-v1014-beam10" \
---ann_path "six_work_mimic_cxr_annotation_similar_case_v0702_v0826.json" \
+--ann_path "cogaze_mimic_cxr_annotation_similar_case_v0702_v0826.json" \
 --images_dir "files" \
 --view_position_path "/MIMIC-CXR/view-positions-dict.json" \
 --eye_gaze_dir "/MIMIC-Eye-Gaze-Heatmap/" \
@@ -19,7 +19,7 @@ CUDA_VISIBLE_DEVICES=1 python ../main_v0702_v0826.py \
 --learning_rate 5.0e-5 \
 --save_best_model "yes" \
 --save_last_model 'no' \
---load "/results/mimic_cxr/pretrain/finetune_v0702-v0826-pretrain_2025_08_27_15/checkpoint/best_model.pt" \
+--load "https://huggingface.co/MK-runner/CoGaze/blob/main/mimic_pretrain_best_model.pt" \
 --ckpt_zoo_dir "/checkpoints/" \
 --num_beams 10 \
 --max_epochs 30
